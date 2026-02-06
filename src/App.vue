2fd60ca958c170e73e317a67eec807d6a2a30756
Diwanul <DiwanulHowl@gmail.com>
2026-02-06 08:59:49 +0800

    chore: Update tooling for Node.js 24 compatibility

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,6 @@
 </template>
 
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
-
-const router = useRouter()
 </script>
 
 <style scoped>
--- src/views/GameBoard.vue
+++ src/views/GameBoard.vue
@@ -6,9 +6,6 @@
 </template>
 
 <script setup lang="ts">
-import { useGameStore } from '../stores/gameStore'
-
-const gameStore = useGameStore()
 </script>
 
 <style scoped>
